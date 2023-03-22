883849a681e504779635dc977561d90d0a507662
Mounika Kankanti Vangala <mounikakanakant@oakland.edu>
2023-03-22 00:44:21 -0400

    Vue integration and frontend dummy pages

---
+++ vue-js/vue-frontend/src/views/HouseSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>HouseSearch</h3>
+  <router-link to="/">Go Back</router-link>
+  <p>City : {{ message }}</p>
+  <input v-model="message" placeholder="edit me" />
+</template>
+
+<script>
+export default {
+  name: 'HouseSearch',
+  inheritAttrs: false, // disable 'non-props' warning
+};
+</script>
